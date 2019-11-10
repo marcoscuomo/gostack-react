4ed17a7e7ee692554fc6f937fa32505b23c9ecac
bytecore@gmail.com <marcos@mojumob.com.br>
2019-11-10 04:26:57 -0300

    Criando um componente raiz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const soma = (a,b) => a + b;
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
-alert(soma(1,6));
+// Primeira versão
+// render(<h1>Hello World</h1>, document.getElementById('app'));
+
+// Segunda versão
+render(<App />, document.getElementById('app'));
