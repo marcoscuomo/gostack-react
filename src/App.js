85f8959aa11c6f55328d68f2dbd1bf0b9bd6e49a
bytecore@gmail.com <marcos@mojumob.com.br>
2019-11-10 19:19:39 -0300

    Class componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import profile from './assets/img1.jpeg';
+// import profile from './assets/img1.jpeg';
+import TechList from './components/TechList';
 
 function App() {
     // return <h1>Hello Mojumob</h1>
-    return <img src={profile} />
+    // return <img src={profile} />
+    return <TechList />
 }
 
 export default App;
