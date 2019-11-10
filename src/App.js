7c747373f7f844354441e08489a93ce8c2c088d2
bytecore@gmail.com <marcos@mojumob.com.br>
2019-11-10 04:48:04 -0300

    Importando imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
+import profile from './assets/img1.jpeg';
 
 function App() {
-    return <h1>Hello Mojumob</h1>
+    // return <h1>Hello Mojumob</h1>
+    return <img src={profile} />
 }
 
 export default App;
