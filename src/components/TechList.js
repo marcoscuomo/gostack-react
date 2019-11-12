c681959bc8c7bdc1f34930d0e56cf63defa6a5a5
bytecore@gmail.com <marcos@mojumob.com.br>
2019-11-12 14:22:28 -0300

    Removendo itens do estado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -28,19 +28,29 @@ class TechList extends Component {
         });
     }
 
+    handleDelete = (tech) => {
+        // console.log(tech);
+        this.setState({techs: this.state.techs.filter(t => t !== tech)});
+    }
+
     render() {
         
         return (
             <form onSubmit={this.handleSubmit}>
                 {/* <h1>{this.state.newTech}</h1> */}
                 <ul>
-                    {this.state.techs.map(tech => <li key={tech}>{tech}</li> )}
+                    {this.state.techs.map(tech => (
+                        <li key={tech}>
+                            {tech}
+                            <button onClick={ () => this.handleDelete(tech)} type="button">Remover</button>
+                        </li>
+                    ))}
                 </ul>  
                 <input 
                 type="text" 
                 onChange={this.handleInputChange} 
                 value={this.state.newTech} />
-                <button type="submit">Enviar</button>   
+                <button type="submit" >Enviar</button>   
             </ form>
         );
         
